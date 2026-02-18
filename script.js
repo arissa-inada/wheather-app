4f4b1cf1d9e3c66b0d82a52476b39a549c468179
arissa-inada <arissa.devft@gmail.com>
2026-02-18 05:05:21 -0300

    Primeira versão do Weather App

---
+++ script.js
@@ -0,0 +1,67 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+
+    const APIKey = '9ac113f93cc6407094f71130261802'; // coloque sua key da WeatherAPI
+    const city = document.querySelector('.search-box input').value;
+
+    if (city === '')
+        return;
+
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${city}&lang=pt`)
+        .then(response => response.json())
+        .then(json => {
+
+            if (json.error) {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
+
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+
+            const condition = json.current.condition.text;
+
+            if (condition.includes('Sol') || condition.includes('Clear')) {
+                image.src = 'clear.png';
+            } else if (condition.includes('Chuva') || condition.includes('Rain')) {
+                image.src = 'rain.png';
+            } else if (condition.includes('Neve')) {
+                image.src = 'snow.png';
+            } else if (condition.includes('Nublado') || condition.includes('Cloud')) {
+                image.src = 'cloud.png';
+            } else if (condition.includes('Névoa') || condition.includes('Mist')) {
+                image.src = 'mist.png';
+            } else {
+                image.src = 'cloud.png';
+            }
+
+            temperature.innerHTML = `${parseInt(json.current.temp_c)}<span>°C</span>`;
+            description.innerHTML = `${json.current.condition.text}`;
+            humidity.innerHTML = `${json.current.humidity}%`;
+            wind.innerHTML = `${parseInt(json.current.wind_kph)} Km/h`;
+
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '590px';
+
+        });
+
+});
+
